6e01fea52e01875fe88cd43f4d2ea86cdcd4fee9
stefanyGRS05 <24006383@galileo.edu>
2024-08-16 21:51:28 -0600

    tarea2

---
+++ Back/Operaciones.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operaciones.Back
+{
+    class Operaciones
+    {
+
+
+        //-------------------------SUMAS
+        public double Suma(double valor1, double valor2)
+        {
+            
+            return valor1+valor2;
+        }
+
+        public double Suma(double valor1, double valor2, double valor3)
+        {
+            return valor1 + valor2 + valor3;
+        }
+
+        public double Suma(double valor1, double valor2, double valor3, double valor4)
+        {
+            return valor1 + valor2 + valor3 + valor4;
+        }
+
+
+        //-------------------------RESTAS
+
+        public double Resta(ref double valor1, ref double valor2)
+        {
+            return valor1 - valor2;
+        }
+        public double Resta(ref double valor1, ref double valor2, ref double valor3)
+        {
+            return valor1 - valor2 - valor3;
+        }
+
+        //-------------------------DIVISION
+        public double Division(ref double valor1, ref double valor2)
+        {
+            return valor1 / valor2;
+        }
+
+        //-----------------------MULTIPLICACIÓN
+        public double Multiplicacion(double valor1, double valor2)
+        {
+            return valor1 * valor2;
+        }
+        public double Multiplicacion(double valor1, double valor2, double valor3)
+        {
+            return valor1 * valor2 * valor3;
+        }
+        public double Multiplicacion(double valor1, double valor2, double valor3, double valor4)
+        {
+            return valor1 * valor2 * valor3 * valor4;
+        }
+
+
+
+    }
+}
---
+++ Front/Program.cs
@@ -0,0 +1,44 @@
+﻿using operaciones.Back;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+        //----------------------sumas
+        Operaciones sumas = new Operaciones();
+        Console.WriteLine("-------------SUMAS-------------");
+        Console.WriteLine("El resultado de la suma es "+ sumas.Suma(5, 10));
+        Console.WriteLine("El resultado de la suma es "+ sumas.Suma(1, 10, 15));
+        Console.WriteLine("El resultado de la suma es "+ sumas.Suma(5, 10, 15, 20));
+
+        //---------------------restas
+        Operaciones restas = new Operaciones();
+        double valorSum1 = 5;
+        double valorSum2 = 10;
+        double valorSum3 = 15;
+        Console.WriteLine("-------------RESTAS-------------");
+        Console.WriteLine("El resultado de la resta es "+ restas.Resta(ref valorSum1, ref valorSum2));
+        Console.WriteLine("El resultado de la resta es "+ restas.Resta(ref valorSum1, ref valorSum2, ref valorSum3));
+
+        //----------------------divisiones
+        Operaciones division = new Operaciones();
+        double valorRes1 = 5;
+        double valorRes2 = 10; 
+        Console.WriteLine("-------------DIVISION-------------");
+        Console.WriteLine("El resultado de la división es "+ division.Division(ref valorRes1,ref valorRes2));
+
+        //----------------------multiplicacion
+        Operaciones multiplicacion = new Operaciones();
+        Console.WriteLine("-------------MULTIPLICACION-------------");
+        Console.WriteLine("El resultado de la multiplicación es "+ multiplicacion.Multiplicacion (5, 10));
+        Console.WriteLine("El resultado de la multiplicación es "+ multiplicacion.Multiplicacion(1, 10, 15));
+        Console.WriteLine("El resultado de la multiplicación es "+ multiplicacion.Multiplicacion(5, 10, 15, 20));
+
+
+
+
+    }
+}
